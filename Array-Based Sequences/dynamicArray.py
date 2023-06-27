275de36a936bbb59dafe3cf870979c499d8268cf
Luis Kellermann <lekellerm95@gmail.com>
2023-06-27 14:43:28 -0400

    implemented __add__ function and set_n helper function

--- Array-Based Sequences/dynamicArray.py
+++ Array-Based Sequences/dynamicArray.py
@@ -249,6 +249,25 @@ class DynamicArray:
             self._A[self._n] = other_array[i]  # assigning element to the next available position
             self._n += 1  # update size of array
 
+    def __add__(self, other_array):
+        """
+        Concatenates two arrays together, and returns result as new DynamicArray
+        Runs in O(n) time, linear where n is the total number of elements in both arrays
+        :param other_array: Array to be concatenated to current array
+        :return Concatenated DynamicArray
+        """
+        new_Array_Size = len(other_array) + self._n  # obtain the size of the new array
+        newArray = DynamicArray(new_Array_Size)  # create a new dynamic array
+        newArray._set_n(new_Array_Size)  # update the number of elements in new array so indexing is possible
+
+        for i in range(self._n):  # iterate through first array and copy elements to new array
+            newArray[i] = self._A[i]
+
+        for i in range(len(other_array)):  # iterate through second array and copy elements to new array
+            newArray[self._n + i] = other_array[i]
+
+        return newArray
+
     def _check_Resize(self):
         """
         Private utility method to handle checking whether resize operation is needed, shrinking or expanding if needed
@@ -272,6 +291,14 @@ class DynamicArray:
 
         self._A = new_Array  # updating the underlying array to now reference newly created (larger) array
         self._capacity = cap  # update capacity of array
+
+    def _set_n(self, numOfElements):
+        """
+        Private utility method to only be used by the __add__ function to update the number of elements
+        in the newly created array from concatenation operation purposes
+        :param numOfElements: Number of elements
+        """
+        self._n = numOfElements
 
     def _make_array(self, cap):
         """
