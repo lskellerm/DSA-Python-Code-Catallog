f52b61bd85595f3fed736f149c39daad732fff64
Luis Kellermann <lekellerm95@gmail.com>
2023-08-17 17:19:34 -0400

    Created linkedList class skeleton

---
+++ LinkedLists/linkedList.py
@@ -0,0 +1,13 @@
+import node
+
+class LinkedList:
+    """
+        A singularly linked list, utilizing the Node ADT to implement Node functionalities
+
+        This implementation uses the Head and Tail pointers to denote the first node in the list and the last node in
+        the list, respectively.
+
+        This class supports basic and some additional linked list functionality.
+
+        Tested using the unit tests in linkedListTest.py
+    """
