ecca38f941bb1de7ceaf79087526b2cf878aa723
Luis Kellermann <lekellerm95@gmail.com>
2023-08-18 15:09:58 -0400

    Implemented constructor, __len__() , is_Empty() functions

--- LinkedLists/linkedList.py
+++ LinkedLists/linkedList.py
@@ -1,5 +1,6 @@
 import node
 
+
 class LinkedList:
     """
         A singularly linked list, utilizing the Node ADT to implement Node functionalities
@@ -11,3 +12,38 @@ class LinkedList:
 
         Tested using the unit tests in linkedListTest.py
     """
+
+    def __init__(self):
+        """
+            Constructor for LinkedList object which creates an initially empty LinkedList
+        """
+        self._size = 0
+        self.head = None
+        self.tail = None
+
+    def __len__(self):
+        """
+        Returns the number of elements stored in the LinkedIst
+        \n Runs in constant, O(1) time
+        :return: The number of nodes in the LinkedList
+        """
+        return self._size
+
+    def is_Empty(self):
+        """
+        Checks if the Linked List is empty
+        \n Runs in constant, O(1) time
+        :return: True if the Linked is empty, False otherwise
+        :raises
+        """
+        return self._size == 0
+
+    def test_Value_At(self, index):
+        """
+        Returns the value at the nth node (0-indexed)
+        \n Runs in linear, O(n) time, where n is the number of nodes in the linked List
+        :param index: The index of the item to retrieve
+        :return: The value of the node at the given index
+        :raises IndexError if the index is out of range
+        """
+
